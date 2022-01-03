e70f5be26bd23934fb32f70ee882b29c7ce92345
Bartosz <bkutkowski@gmail.com>
2022-01-03 22:36:58 +0100

    Initial commit

---
+++ src/main/java/com/example/shop4good/Shop4goodApplication.java
@@ -0,0 +1,13 @@
+package com.example.shop4good;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shop4goodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Shop4goodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shop4good/Shop4goodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shop4good;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shop4goodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
